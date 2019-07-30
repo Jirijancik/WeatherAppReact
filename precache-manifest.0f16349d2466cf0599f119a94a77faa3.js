8d478c4d9f40dedf4b61f21aa9812c51c22cb8e1
jirijancik <jirijancik2@gmail.com>
2019-07-30 22:32:36 +0200

    Updates

---
+++ precache-manifest.0f16349d2466cf0599f119a94a77faa3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d9a9d8a7a137bb0709ffbac92451ea",
+    "url": "/WeatherAppReact/index.html"
+  },
+  {
+    "revision": "400a933256ce20ab40a8",
+    "url": "/WeatherAppReact/static/css/main.44cbee5a.chunk.css"
+  },
+  {
+    "revision": "a3b504ceff639bab8500",
+    "url": "/WeatherAppReact/static/js/2.0b1e0de2.chunk.js"
+  },
+  {
+    "revision": "400a933256ce20ab40a8",
+    "url": "/WeatherAppReact/static/js/main.02b4d694.chunk.js"
+  },
+  {
+    "revision": "215663e4c9fdf09bd55e",
+    "url": "/WeatherAppReact/static/js/runtime~main.a9e0aea6.js"
+  },
+  {
+    "revision": "10a7323a470dda987a100c442930be95",
+    "url": "/WeatherAppReact/static/media/Clear.10a7323a.jpg"
+  },
+  {
+    "revision": "575843f4ee13fa0fb1d2d98b609c492c",
+    "url": "/WeatherAppReact/static/media/Clouds.575843f4.jpg"
+  },
+  {
+    "revision": "b1602a91cb92798988fc633381921993",
+    "url": "/WeatherAppReact/static/media/Drizzle.b1602a91.jpg"
+  },
+  {
+    "revision": "4cbf7c4561c4ab2465bed9b579d05287",
+    "url": "/WeatherAppReact/static/media/Rain.4cbf7c45.jpg"
+  },
+  {
+    "revision": "c8ba198b7c914b7cf8d48de513006511",
+    "url": "/WeatherAppReact/static/media/Sunny.c8ba198b.jpg"
+  },
+  {
+    "revision": "e1ed9fe4fc04752c23c81be33ad8ab18",
+    "url": "/WeatherAppReact/static/media/Thunderstorm.e1ed9fe4.jpg"
+  }
+]);
